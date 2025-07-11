b6dea931fc151d1cb8aa75bd58a5cc891f946a91
Hongbono1 <qwe4787qwe@gmail.com>
2025-07-12 03:54:38 +0900

    fix: /restaurant 경로에서 restaurant.html 서빙 라우터 추가

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,7 +1,14 @@
+// routes/restaurant.js
 import express from "express";
-import { getStoresByRestaurant } from "../controllers/restaurantController.js";
+import path from "path";
+
 const router = express.Router();
 
-router.get("/stores", getStoresByRestaurant);
+router.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "restaurant.html"));
+});
+
+// 기존 API들
+router.get("/:category/stores", ...);
 
 export default router;
