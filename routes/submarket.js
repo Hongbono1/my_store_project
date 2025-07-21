a47b78a48a2ae3e8b893e52c851ea1c23316d157
Hongbono1 <qwe4787qwe@gmail.com>
2025-07-22 00:23:51 +0900

    marketregister.html, routes/submarket.js 수정

--- routes/submarket.js
+++ routes/submarket.js
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 import path from "path";
-import { createMarket } from "../controllers/marketController.js";
+import { createSubmarket } from "../controllers/submarketController.js";
 
 const router = express.Router();
 const upload = multer({ dest: path.join(process.cwd(), "public", "uploads/") });
