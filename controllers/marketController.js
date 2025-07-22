c5ea568f137abcb6302cb88407da97e6fb1cc44b
Hongbono1 <qwe4787qwe@gmail.com>
2025-07-23 05:55:35 +0900

    market 등록/조회 컨트롤러 (네온 컬럼 완전 일치)

--- controllers/marketController.js
+++ controllers/marketController.js
@@ -37,7 +37,7 @@ export async function createMarket(req, res) {
             $1, $2, $3, $4, $5,
             $6, $7, $8, $9, $10,
             $11, $12, $13, $14
-          )
+           ) RETURNING id
         `
         
         const values = [
