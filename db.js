d5d08e478fde77393a1024f8c995cf04b2ab5676
Hongbono1 <qwe4787qwe@gmail.com>
2025-08-07 14:26:22 +0900

    fix: switch to PG* env vars

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+ import { Pool } from "pg";
+ import "dotenv/config";
 
-export const pool = new Pool({
-  host:     process.env.PGHOST,
-  port:     Number(process.env.PGPORT) || 5432,
-  user:     process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  ssl:      { rejectUnauthorized: false },   // Neon's serverless SSL
-});
-export default pool;
+ const pool = new Pool({
+   host:     process.env.PGHOST,
+   port:     Number(process.env.PGPORT) || 5432,
+   user:     process.env.PGUSER,
+   password: process.env.PGPASSWORD,
+   database: process.env.PGDATABASE,
+   ssl:      { rejectUnauthorized: false },   // Neon TLS
+ });
+
+ export default pool;
