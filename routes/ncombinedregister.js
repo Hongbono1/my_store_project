39b34d89f1fce0517c9a10cba4b293ca7b062ec9
Hongbono1 <qwe4787qwe@gmail.com>
2025-09-04 00:46:17 +0900

    ncombinedregister.js: 서브카테고리 가게이미지 안보임 수정

--- routes/ncombinedregister.js
+++ routes/ncombinedregister.js
@@ -1,11 +1,23 @@
-// routes/ncombinedregister.js
 import { Router } from "express";
 import multer from "multer";
 import path from "path";
+import { randomUUID } from "crypto";
 import * as ctrl from "../controllers/ncombinedregisterController.js";
 
 const router = Router();
-const upload = multer({ dest: path.join(process.cwd(), "uploads") });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(process.cwd(), "public/uploads")); // 저장 경로
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname); // 원본 확장자 추출 (.jpg, .png)
+    const name = randomUUID();                   // 랜덤 파일명
+    cb(null, name + ext);                        // 확장자 포함 저장
+  },
+});
+
+const upload = multer({ storage });
 
 // [POST] /combined
 router.post(
