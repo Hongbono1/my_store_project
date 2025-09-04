b092e329423a3adf958bdddef7ab895e2145493f
Hongbono1 <qwe4787qwe@gmail.com>
2025-09-05 04:07:17 +0900

    음식 서브 카테고리 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
     // 동적으로 생성하는 클래스(ex. max-h-[1080px])가 있다면
     'max-h-[1080px]',
   ],
-  theme: { /* … */ },
+  theme: {
+    extend: {
+      height: {
+        38: '9.5rem',   // 152px
+      },
+      spacing: {
+        38: '9.5rem',   // margin/padding에서도 같은 단위 사용 가능
+      },
+    },
+  },
   plugins: [],
 }
