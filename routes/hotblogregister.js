dfce8e03e8713dbf14cad2f2217ad7f598656b90
Hongbono1 <qwe4787qwe@gmail.com>
2025-09-07 23:12:07 +0900

    서버js 중복  수정

--- routes/hotblogregister.js
+++ routes/hotblogregister.js
@@ -1,12 +1,12 @@
-// routes/hotblogregister.js
 import { Router } from "express";
 import multer from "multer";
-import * as ctrl from "../controllers/hotblogregisterController.js";
+import * as ctrl from "../controllers/hotblogregisterController.js"; // ← 네임스페이스 import
 
 const router = Router();
 const upload = multer({ dest: "uploads/" });
 
-console.log("[hotblog routes] exports:", Object.keys(ctrl)); // 시작시 확인
+// 부팅 시 실제 export 확인 (로그에서 'registerHotBlog','getHotBlog' 보여야 정상)
+console.log("[hotblog routes] exports:", Object.keys(ctrl));
 
 router.post("/register", upload.any(), ctrl.registerHotBlog);
 router.get("/:id", ctrl.getHotBlog);
