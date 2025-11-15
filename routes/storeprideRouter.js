df874712a82f9c3a2e7666c671598116d6771d5e
Hongbono1 <qwe4787qwe@gmail.com>
2025-11-16 08:36:28 +0900

    fix: storepride.html에 업종(category) 표시 추가

--- routes/storeprideRouter.js
+++ routes/storeprideRouter.js
@@ -7,6 +7,9 @@ const router = express.Router();
 // 리스트
 router.get("/list", getStorePrideList);
 
+// 슬라이드용 (동일 데이터)
+router.get("/api", getStorePrideList);
+
 // 상세
 router.get("/detail/:id", getStorePrideDetail);
 
