d70fba48820178758654766d2fe82bd28a93b651
Hongbono1 <qwe4787qwe@gmail.com>
2025-11-23 23:36:01 +0900

    fix: move inquiryRouter above JSON parser to fix empty req.body

--- server.js
+++ server.js
@@ -121,6 +121,7 @@ app.use((req, res, next) => {
   });
   next();
 });
+app.use("/api/inquiry", inquiryRouter);
 
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
@@ -143,7 +144,7 @@ app.use("/api/oneword", onewordRouter);
 app.use("/shopping/register", shoppingRegisterRouter);
 app.use("/api/shopping", shoppingDetailRouter);
 app.use("/api/best-pick", bestpickRouter);
-app.use("/api/inquiry", inquiryRouter);
+
 
 // Open Store API 라우트
 app.use("/api/open/register", openregisterRouter);
