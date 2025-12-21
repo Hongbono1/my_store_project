a1e34e74e0c95e2feda489fac261a0a95064a1bd
Hongbono1 <qwe4787qwe@gmail.com>
2025-12-21 20:29:51 +0900

    foodcategory.html 사이드 광고박스 추가 3개만들기 / 사이드 글자 조정

--- server.js
+++ server.js
@@ -373,6 +373,7 @@ app.use(
     extensions: ["html"],
     setHeaders: (res, filePath) => {
       if (filePath.endsWith(".html")) {
+        res.setHeader("Content-Type", "text/html; charset=utf-8");
         res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         res.setHeader("Pragma", "no-cache");
         res.setHeader("Expires", "0");
